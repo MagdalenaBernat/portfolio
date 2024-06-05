e25fca472dd674b7b1c5267f71ecd41876461155
MagdalenaBernat <magdalena.bernat@onet.pl>
2024-06-05 19:41:59 +0200

    responsive >1700px, logo

--- src/js/nav_button_active.js
+++ src/js/nav_button_active.js
@@ -5,10 +5,7 @@ window.onload = function() {
 
     buttons.forEach(button => {
         button.addEventListener('click', function() {
-            // Remove the active class from all buttons
             buttons.forEach(btn => btn.classList.remove('active'));
-
-            // Add the active class to the clicked button
             this.classList.add('active');
         });
     });
