ba50e99d42e0f7334b82e3736d1b487d6d523aa0
MagdalenaBernat <magdalena.bernat@onet.pl>
2024-11-04 01:49:54 +0100

    hamburger menu

---
+++ src/js/hamburger_menu.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
